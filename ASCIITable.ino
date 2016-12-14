f77cda0d0de3dbdcb102e47ed67fc38f1eb7d643
Güray Gürkan <GurayGurkan@users.noreply.github.com>
2016-12-14 20:52:07 +0200

    Add files via upload

---
+++ ASCIITable.ino
@@ -0,0 +1,27 @@
+/*   ASCII table
+ Prints out byte values in all possible formats:
+ * as raw binary values
+ * as ASCII-encoded decimal, hex, octal, and binary values  */
+void setup() {
+  Serial.begin(9600);
+  Serial.println("ASCII Table ~ Character Map");
+}
+
+int thisByte = 33;
+
+void loop() {
+  Serial.write(thisByte);
+  Serial.print(", dec: ");
+  Serial.print(thisByte);
+  Serial.print(", hex: ");
+  Serial.print(thisByte, HEX);
+  Serial.print(", oct: ");
+  Serial.print(thisByte, OCT);
+  Serial.print(", bin: ");
+  Serial.println(thisByte, BIN);
+  if (thisByte == 126) {    // you could also use if (thisByte == '~') {
+    while (true) {
+      continue;  }
+  }
+  thisByte++;
+}
