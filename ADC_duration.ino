658b7be0606a401a9b73400b716749cb3c3453a5
Güray Gürkan <GurayGurkan@users.noreply.github.com>
2016-12-14 20:50:23 +0200

    Add files via upload

---
+++ ADC_duration.ino
@@ -0,0 +1,12 @@
+byte LEDpin = 13;
+unsigned long ADCval;
+
+void setup() {
+  pinMode(LEDpin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LEDpin, HIGH);
+  ADCval = analogRead(A0);
+  digitalWrite(LEDpin, LOW);
+}
---
+++ ST_HMC5883L_test.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <HMC5883L.h>
+
+HMC5883L compass;
+unsigned long START;
+void setup()
+{
+  Wire.begin(); 
+  Serial.begin(9600);
+  compass = HMC5883L();
+  compass.SetMeasurementMode(Measurement_Continuous);
+   
+  //compass.SetBiasConfig(0);
+  //compass.SetScale(1.3);
+  START = millis();
+}
+
+void loop()
+{
+   if ((millis()- START)<4000)
+   {
+    MagnetometerRaw raw = compass.ReadRawAxis();
+    
+    Serial.print(raw.XAxis);
+    Serial.print(",");
+    Serial.print(raw.YAxis);
+    Serial.print(",");
+    Serial.println(raw.ZAxis);
+   }
+   delay(64);
+}
