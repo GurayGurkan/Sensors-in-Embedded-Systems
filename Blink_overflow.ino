0e59e8e3616d716f389c1970548a4e4beb9c2026
Güray Gürkan <GurayGurkan@users.noreply.github.com>
2016-12-14 20:50:56 +0200

    Add files via upload

---
+++ Blink_overflow.ino
@@ -0,0 +1,26 @@
+int pin1 = 13;
+
+unsigned long duration = 1000;
+
+void setup() {
+ pinMode(pin1, OUTPUT);
+ blinker(pin1, 20, 20, 260);
+}
+
+void loop()
+{
+  
+  
+}
+
+void blinker(int pin, unsigned long T_ON, unsigned long T_OFF,
+             byte repeats)
+{
+  for (int c = 0; c < repeats; c++)
+  {
+    digitalWrite(pin, HIGH);
+    delay(T_ON);
+    digitalWrite(pin, LOW);
+    delay(T_OFF);
+  }
+}
